94d3efd534276ea1f27984aeda675b65cd94f84d
Nakshatra6350 <nakshatragarg678@gmail.com>
2024-04-20 13:17:26 +0530

    added chatbot

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import { Provider } from "react-redux";
 import store from "./store";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import Kommunicate from "@kommunicate/kommunicate-chatbot-plugin";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+Kommunicate.init("bcc7c598545c26189d5f06a8e89d3e57", {
+  automaticChatOpenOnNavigation: true,
+  popupWidget: true,
+});
 root.render(
   <React.StrictMode>
     <Provider store={store}>
